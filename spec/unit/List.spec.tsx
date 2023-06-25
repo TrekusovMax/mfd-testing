767cfdb119e106970e56f5c3c829e56a401a0822
Maxim <maxtrekusovsite@gmail.com>
2023-06-25 22:53:41 +0300

    Fit: добавлен тест List

--- spec/unit/List.spec.tsx
+++ spec/unit/List.spec.tsx
@@ -1,41 +1,61 @@
-import { render, screen } from "@testing-library/react";
-import { List } from "src/components/List";
+import { render, screen } from '@testing-library/react'
+import { App } from 'src/App'
+import { List } from 'src/components/List'
+import ue from '@testing-library/user-event'
 
-it("отображение списка задач", () => {
-  const onDelete = jest.fn();
-  const onToggle = jest.fn();
+it('отображение списка задач', () => {
+  const onDelete = jest.fn()
+  const onToggle = jest.fn()
 
   const items: Task[] = [
     {
-      id: "1",
-      header: "купить хлеб",
+      id: '1',
+      header: 'купить хлеб',
       done: false,
     },
     {
-      id: "2",
-      header: "купить молоко",
+      id: '2',
+      header: 'купить молоко',
       done: false,
     },
     {
-      id: "3",
-      header: "выгулять собаку",
+      id: '3',
+      header: 'выгулять собаку',
       done: true,
     },
-  ];
+  ]
 
   const { rerender, asFragment } = render(
-    <List items={items} onDelete={onDelete} onToggle={onToggle} />
-  );
-  const firstRender = asFragment();
-  
-  items.pop();
+    <List items={items} onDelete={onDelete} onToggle={onToggle} />,
+  )
+  const firstRender = asFragment()
 
-  rerender(<List items={items} onDelete={onDelete} onToggle={onToggle} />);
-  const secondRender = asFragment();
+  items.pop()
 
-  expect(firstRender).toMatchDiffSnapshot(secondRender);
-});
+  rerender(<List items={items} onDelete={onDelete} onToggle={onToggle} />)
+  const secondRender = asFragment()
 
-it("Список содержит не больше 10 невыполненных задач", () => {
+  expect(firstRender).toMatchDiffSnapshot(secondRender)
+})
 
-});
+it('Список содержит не больше 10 невыполненных задач', async () => {
+  const userEvent = ue.setup({
+    advanceTimers: jest.advanceTimersByTime,
+  })
+
+  render(<App />)
+
+  for (let i = 0; i < 10; i++) {
+    const inputEl = screen.getByRole('textbox')
+    const addBtnEl = screen.getByAltText(/Добавить/i)
+
+    await userEvent.clear(inputEl)
+    await userEvent.type(inputEl, `Задача номер ${i + 1}`)
+    await userEvent.click(addBtnEl)
+  }
+  const list = screen.getByRole('list')
+  const items = list.querySelectorAll('li')
+
+  expect(items).toHaveLength(10)
+})
+
