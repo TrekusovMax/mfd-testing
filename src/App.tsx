5e376baaeba412c8bf34ce7d93bc91ac9e63f155
Maxim <maxtrekusovsite@gmail.com>
2023-06-25 22:13:47 +0300

    Fit: добавлен компонент Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { store } from "./store/configureStore";
+import { store } from './store/configureStore'
 
-import "./styles.css";
-import { NewTaskBar } from "./modules/NewTaskBar";
-import { TaskList } from "./modules/TaskList";
-import { Provider } from "react-redux";
-import { NotifierContainer } from "./modules/NotifierContainer";
-
+import './styles.css'
+import { NewTaskBar } from './modules/NewTaskBar'
+import { TaskList } from './modules/TaskList'
+import { Provider } from 'react-redux'
+import { NotifierContainer } from './modules/NotifierContainer'
+import { Filter } from './modules/Filter'
 
 export const App = () => {
   return (
     <div className="root-container">
       <Provider store={store}>
         <h3>Список задач</h3>
+        <Filter />
         <NewTaskBar />
         <TaskList />
         <NotifierContainer />
       </Provider>
     </div>
-  );
-};
+  )
+}
+
